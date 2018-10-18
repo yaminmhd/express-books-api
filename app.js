ba66f1d38277353006a787b13a9a9e5a3ef87391
yaminmhd <mdyamin.drzee@gmail.com>
2018-10-18 19:04:38 +0800

    update scripts in package.json  and app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require("express");
 const path = require("path");
-const favicon = require("serve-favicon");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
@@ -12,8 +11,6 @@ const bodyParser = require("body-parser");
 const index = require("./routes/index");
 const books = require("./routes/books");
 const authors = require("./routes/authors");
-const mongoose = require('mongoose');
-const seedDB = require('./seeds');
 const app = express();
 
 // view engine setup
@@ -23,7 +20,7 @@ app.set("view engine", "ejs");
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
